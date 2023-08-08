b26e4a8c5e0e1070b9c9bca057ff0053ef15ecea
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-08-08 10:58:41 +0000

    Mapeo modificado

--- src/models.py
+++ src/models.py
@@ -26,18 +26,21 @@ class Follower(Base):
     id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
-
-class Media(Base):
-     __tablename__ = 'media'
-     id = Column(Integer, primary_key=True)
-     url =  Column(String(250),nullable=False)
-     post_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(User)
 
 class Post(Base):
      __tablename__ = 'post'
      id = Column(Integer, primary_key=True)
      usre_id = Column(Integer, ForeignKey('user.id'))
      title = Column(String(50), nullable=False) 
+     user = relationship(User)
+     
+class Media(Base):
+     __tablename__ = 'media'
+     id = Column(Integer, primary_key=True)
+     url =  Column(String(250),nullable=False)
+     post_id = Column(Integer, ForeignKey('post.id'))
+     post = relationship(Post)
     
 class Favorites(Base):
      __tablename__ = 'favorites'
@@ -45,12 +48,15 @@ class Favorites(Base):
      user_id = Column(Integer, ForeignKey('user.id'))
      name = Column(String(50), nullable=False)
      url =  Column(String(250),nullable=False)
+     user = relationship(User)
 
 class Comment(Base):
      __tablename__ = 'comment'
      id = Column(Integer, primary_key=True)
      author_id = Column(Integer, ForeignKey('user.id'))
      post_id = Column(Integer, ForeignKey('post.id'))
+     user = relationship(User)
+     post = relationship(Post)
 
 class Characters(Base):
     __tablename__ = 'characters' 
@@ -58,18 +64,21 @@ class Characters(Base):
     names = Column(String(50), nullable=False)
     url =  Column(String(250),nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 class Planets(Base):
      __tablename__ = 'planets'
      id = Column(Integer, primary_key=True)
      names = Column(String(50), nullable=False) 
      user_id = Column(Integer, ForeignKey('user.id'))
+     user = relationship(User)
 
 class Starship(Base):
      __tablename__ = 'starship'
      id = Column(Integer, primary_key=True)
      names = Column(String(50), nullable=False) 
      user_id = Column(Integer, ForeignKey('user.id'))
+     user = relationship(User)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
